4503e273a263151e4ae5890afd8cce626fae5723
Ahmed Hamad <60454980+ahmedhamad-okkio@users.noreply.github.com>
2021-08-27 17:46:43 +0100

    HealthComponent Refactored
    Removed Client KillActor function as it is not used anymore and cleaned the file spacing up

--- Source/WokeAndShoot/GameComponents/Character/CharacterComponenets/HealthComponent.cpp
+++ Source/WokeAndShoot/GameComponents/Character/CharacterComponenets/HealthComponent.cpp
@@ -7,32 +7,15 @@
 #include "../../PlayerController/WokeAndShootPlayerController.h"
 
 
-// Sets default values for this component's properties
 UHealthComponent::UHealthComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 	HealthPoints = MaxHealth;
-	// ...
 }
 
-
-// Called when the game starts
 void UHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
-	// ...
-	
-}
-
-
-// Called every frame
-void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
 void UHealthComponent::ApplyDamage(float Damage, AController* Killer) 
@@ -41,26 +24,7 @@ void UHealthComponent::ApplyDamage(float Damage, AController* Killer)
 	HealthPoints =	FMath::Max(DamagedHealth, 0.f);
 	if(HealthPoints == 0)
 	{
-		// if(GetOwnerRole() == ROLE_AutonomousProxy)
-		// {
-
-		// 	GLog->Log("AUTO PROXY");
-		// }
-		// if(GetOwnerRole() == ROLE_Authority)
-		// {
-
-		// 	GLog->Log("AUTHORITY");
-		// }
-		if(GetOwnerRole() == ROLE_SimulatedProxy)
-		{
-			
-		}
-		if(Killer)
-		{
-			// GLog->Log(Killer->GetName());
-		}
 		Server_KillActor(Killer);
-		Client_KillActor(Killer);
 	}
 }
 
@@ -70,31 +34,19 @@ void UHealthComponent::ApplyHeal(float Heal)
 	HealthPoints =	FMath::Min(HealedHealth, MaxHealth);
 }
 
-
 void UHealthComponent::Server_KillActor(AController* Killer) 
 {
 	if(GetOwnerRole() != ROLE_Authority){return;}
+
 	APawn* PlayerPawn =  Cast<APawn>(GetOwner());
 	if(PlayerPawn == nullptr){return;}
+
 	AController* PlayerController = PlayerPawn->GetController();
 	if(PlayerController == nullptr){return;}
-	AWokeAndShootGameMode* Gamemode = GetWorld()->GetAuthGameMode<AWokeAndShootGameMode>();
-	if(Gamemode != nullptr)
+
+	
+	if(AWokeAndShootGameMode* Gamemode = GetWorld()->GetAuthGameMode<AWokeAndShootGameMode>())
 	{
 		Gamemode->PawnKilled(PlayerController, Killer);
 	}	
-}
-
-void UHealthComponent::Client_KillActor(AController* Killer) 
-{
-	AWokeAndShootPlayerController* KilledPlayerController = Cast<AWokeAndShootPlayerController>(GetOwner()->GetInstigatorController());
-	if(KilledPlayerController == nullptr){return;}
-	if(KilledPlayerController->IsLocalController())
-	{
-		APawn* PlayerPawn =  Cast<APawn>(GetOwner());
-		if(PlayerPawn != nullptr)
-		{
-			// PlayerPawn->DetachFromControllerPendingDestroy();
-		}
-	}
 }
